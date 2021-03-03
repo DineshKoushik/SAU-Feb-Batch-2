0fe2db48b37240974bb1793f2956c07c5bb84640
Dinesh <dinesh@gmail.com>
2021-03-03 22:43:08 +0530

    Hibernate Morning Done

---
+++ Hibernate-Morning/src/main/java/com/nik/Config.java
@@ -0,0 +1,15 @@
+package com.nik;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Config {
+	public static Session getSession() {
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        return session;
+    }
+
+}
---
+++ Hibernate-Morning/src/main/java/com/nik/Employee.java
@@ -0,0 +1,68 @@
+package com.nik;
+
+public class Employee {
+
+	private int id;
+	private String firstname;
+	private String lastname;
+	private int age;
+	private int salary;
+	private String dob;
+	private String designation;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+}
